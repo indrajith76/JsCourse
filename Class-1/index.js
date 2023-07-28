3b3c15b0d6ee5ca4617879213431c60c09c57861
indrajith76 <indrajithgoswami@gmail.com>
2023-07-28 13:29:51 +0600

    added first class

--- Class-1/index.js
+++ Class-1/index.js
@@ -1,1 +1,58 @@
-asdf
+// 1. Version Check command -> node -v
+// 2. Data types
+// 3. Keyword
+// 4. variable
+// var name = "mozid";
+// // console.log(name)
+// var roll = 12;
+// var isStudent = false;
+// console.log(name, roll, isStudent)
+
+// var true = ''; wrong
+// var first name = "omuk"; wrong
+// var 'name' = "ali"; wrong
+// var 12clubs = "asldjf"; // wrong
+
+// var snake_case = "hello"; right
+// var camelCase = "hello"; right
+// var PascalCase = "hello"; right
+
+
+// 5. Math Oparations
+var num1 = 12;
+var num2 = 13
+// var total = num1 + num2;
+// console.log("Total:",total)
+
+// var sub = num2 - num1;
+// console.log(sub)
+
+// var mult = num1 * num2;
+// console.log(mult)
+
+var division = num2 / num1;
+// console.log(division.toFixed(4))
+var bhagsas = num2 % num1;
+// console.log(bhagsas);
+
+var IntgerNumber = parseInt(division)
+// console.log(IntgerNumber)
+
+var num3 = 2.99;
+// console.log(parseInt(num3));
+
+var num4 = 1.00;
+// console.log(parseFloat(num4))
+
+var firstName = "Amin";
+var age = 12;
+var isStu = true;
+
+// console.log("My name is", firstName,".", "I am ",age," years old.", " I'm a student:", isStu);
+// console.log(`My name is ${firstName}. I am ${age} years old. I am a student: ${isStu}`)
+
+// type of var :
+// -------------
+console.log("firstName", "type is", typeof firstName);
+console.log(" age type is :", typeof age)
+console.log("isStu type is :", typeof isStu)
