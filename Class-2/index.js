2fce58440f9b5452c4d499699667123d20a11e89
indrajith76 <indrajithgoswami@gmail.com>
2023-08-01 15:24:03 +0600

    added Class 2

---
+++ Class-2/index.js
@@ -0,0 +1,73 @@
+// 1. Shorthand
+var price = 30;
+// price += 10; price = price + 10;
+// price++; -> price = price + 1;
+
+// price--;
+// price -= 10;
+
+// price *= 10;
+
+// price /= 10;
+// console.log(price);
+
+// 2. Arrary
+
+var weight = [31, 45, 55, 66];
+// console.log(weight.length);
+// console.log(weight[2]);
+// weight[1] = 400;
+// console.log(weight)
+// console.log(weight.indexOf(66))
+
+var friendsName = ["JODU", "MODU", "RAM", "Sham", "Rahim", "Karim"];
+// console.log(friendsName.length);
+// console.log(friendsName[0])
+// friendsName[2] = "Hablu";
+// console.log(friendsName);
+// console.log(friendsName.indexOf("Rahim"));
+
+// friendsName.push("Montu", "Jhontu", "Gittu"); <-- add data
+
+// friendsName.pop()   <- remove a data from array
+
+// friendsName.shift() <- remove first data from an array
+
+// friendsName.unshift("Dalim", "Halim", "Jalim") <- add data
+
+// console.log(friendsName);
+
+// 3. conditional/comparison Operators
+// console.log(10 > 9)
+// console.log(10 < 9)
+// console.log(10 == 9);
+// console.log(9 == 9);
+// console.log(10 >= 9);
+// console.log(9 <= 10);
+
+// 4. Conditional
+/* var taka = 300;
+var watchPrice = 1000;
+var normalWatch = 400;
+
+if(taka == watchPrice || taka > normalWatch){
+    console.log("You can buy watch.");
+}
+else {
+    console.log("YOu can't buy this watch.");
+} */
+
+
+/* var mark = 32;
+if(mark >= 80) {
+    console.log("A+")
+}
+else if(mark >= 60 && mark < 80){
+    console.log("A-");
+}
+else if(mark >= 33 && mark <= 40) {
+    console.log("D")
+}
+else {
+    console.log("Fail");
+} */
