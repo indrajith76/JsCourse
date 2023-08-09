b1e8e36fe3fcff0d48502a4987be1c5def8ea29d
indrajith76 <indrajithgoswami@gmail.com>
2023-08-09 14:08:18 +0600

    added Class-3

---
+++ Class-3/index.js
@@ -0,0 +1,98 @@
+// while loop
+// var startingNum = 1;
+
+/* while(startingNum <= 10) {
+    console.log(startingNum);
+    // startingNum++;
+    // startingNum = startingNum+1;
+    // startingNum+=1;
+} */
+
+/* var startingNum = 10;
+
+while (startingNum >= 1) {
+  console.log(startingNum);
+  startingNum--;
+}   */
+
+// print even numbers using while loop
+/* var num = 0;
+while(num <= 10){
+    console.log(num);
+    num = num+2;
+} */
+
+// print odd number using while loop
+/* var num = 1;
+while(num <= 10){
+    console.log(num);
+    num = num + 2;
+} */
+// for loop
+
+/* for (var startingNum = 1; startingNum < 10; startingNum++) {
+  console.log(startingNum);
+}
+ */
+
+/* for (var i = 10; i >= 0; i--) {
+  console.log(i);
+} */
+
+// print even number using for loop
+/* for (var i = 0; i <= 10; i+=2) {
+  console.log(i);
+} */
+
+// print odd number using for loop
+/* for (var i = 1; i <= 10; i += 2) {
+  console.log(i);
+} */
+
+/* var fruits = ['mango','katal','lichu','jam','anaros','komola','angur','orenge'];
+
+for(var i = 0; i < fruits.length; i++){
+    console.log(fruits[i], i);
+} */
+
+/* for (var i = fruits.length - 1; i >= 0; i--) {
+    const fruit = fruits[i];
+    console.log(fruit, i);
+} */
+
+// uses of break
+/* var mark = 0;
+while (mark <= 100) {
+  if (mark == 33) {
+    console.log("Pass mark  hoitasa:",mark);
+    break;
+  }
+  console.log(mark);
+  mark++;
+} */
+
+var fruits = [
+  "mango",
+  "katal",
+  "lichu",
+  "jam",
+  "anaros",
+  "komola",
+  "angur",
+  "orenge",
+];
+
+/* for (var i = 0; i < fruits.length; i++) {
+  if (fruits[i] == "anaros") {
+    console.log('amr favorite',fruits[i]);
+    break;
+  }
+  console.log(fruits[i]);
+} */
+
+for (var i = 0; i < fruits.length; i++) {
+    if (fruits[i] == "anaros") {
+      console.log('amr favorite',fruits[i]);
+      continue;
+    } 
+  }
